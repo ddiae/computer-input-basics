de049e260e109b886fced17dd12c1e3030ea4752
jiaezzang <visveryver2@gmail.com>
2026-05-10 20:17:00 +0900

    chore: tailwind-merge, clsx 설치 및 cn 유틸 추가

---
+++ src/lib/cn.ts
@@ -0,0 +1,12 @@
+import clsx, { type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * A utility to merge Tailwind class names with conditional class composition.
+ *
+ * @example
+ * cn('px-4', isActive && 'bg-blue-500', 'text-sm')
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
